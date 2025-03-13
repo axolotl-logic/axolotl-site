846103accc97b63cf2aee13a95459f9faf14793f
Autumn Green <me@axolotl-logic.io>
2025-03-13 11:34:07 -0400

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Axolotl-Logic",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="min-h-dvh bg-slate-950 text-neutral-100 bg-[url(/images/background.png)] flex flex-col">
         {children}
+        <Analytics />
       </body>
     </html>
   );
